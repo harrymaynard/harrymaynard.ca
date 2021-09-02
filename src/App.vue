a614b195122ddfc11c410fbb64f9d3073a6089c7
Harry Maynard <maynard321@gmail.com>
2021-09-01 20:18:29 -0700

    Added links and icons to home page.

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,5 @@
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="page">
-    <h1 class="title">Harry Maynard</h1>
+    <div>
+      <h1 class="title">Harry Maynard</h1>
+      <div class="links">
+        <a href="https://www.linkedin.com/in/harrymaynard/" target="_blank">
+          <img src="../assets/linkedin-icon.svg" alt="LinkedIn" />
+        </a>
+        <a href="https://github.com/harrymaynard" target="_blank">
+          <img src="../assets/github-icon.svg" alt="GitHub" />
+        </a>
+        <a href="https://twitter.com/theharrymaynard" target="_blank">
+          <img src="../assets/twitter-icon.svg" alt="Twitter" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'Home'
-}
-</script>
 
 <style scoped>
 .page {
   height: 100vh;
-  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 .title {
   font-size: 48px;
   cursor: default;
-  -moz-user-select: none;
-  -webkit-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+}
+.links {
+  margin-top: 30px;
+}
+.links img {
+  width: 40px;
+  height: 40px;
+  margin: 0 10px;
 }
 </style>
