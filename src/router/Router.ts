28a8afb8df0b51a67efb4f46a852581931cda6e5
Harry Maynard <maynard321@gmail.com>
2023-08-24 23:09:21 -0700

    `HomeView` rename and added animations.

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/views/HomePage.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomePage
+    component: HomeView
   },
 ]
 
