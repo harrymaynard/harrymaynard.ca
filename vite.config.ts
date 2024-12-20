0ab2aa329edb294e29f8000f530171594c8142af
Harry Maynard <maynard321@gmail.com>
2024-12-19 21:33:36 -0800

    Update Vite configuration for SCSS preprocessor options and change Sass dependency to sass-embedded

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
 })
