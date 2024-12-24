1f6537e04f2e38b9ced2afda83140118774a053f
Harry Maynard <maynard321@gmail.com>
2024-12-23 21:51:12 -0800

    Add `ThunderstormParticleEntity` and `ThunderstormSkyEntity` classes for thunderstorm effects

---
+++ src/canvas/entities/ThunderstormSkyEntity.ts
@@ -0,0 +1,43 @@
+import { AbstractEntity } from '@/canvas/entities/AbstractEntity'
+import { ThunderstormParticleEntity } from './ThunderstormParticleEntity'
+import { EntityEventType } from '../enums/EntityEventType'
+
+/**
+ * ThunderstormSkyEntity class which handles the thunderstorm sky background entities.
+ */
+export class ThunderstormSkyEntity extends AbstractEntity {
+  private _thunderstormParticle: ThunderstormParticleEntity | null = null
+
+  constructor(params) {
+    super(params)
+
+    this._createThunderstormParticle = this._createThunderstormParticle.bind(this)
+
+    this._createThunderstormParticle()
+  }
+
+  private _createThunderstormParticle(): void {
+    if (this._thunderstormParticle) {
+      this._thunderstormParticle.removeEventListener(EntityEventType.ExitFrame, this._createThunderstormParticle)
+      this.removeChild(this._thunderstormParticle)
+    }
+
+    this._thunderstormParticle = new ThunderstormParticleEntity({
+      context: this.context,
+      viewport: this.viewport,
+      position: {
+        x: 0,
+        y: 0,
+        width: this.viewport.width,
+        height: this.viewport.height,
+      },
+    })
+    this._thunderstormParticle.addEventListener(EntityEventType.ExitFrame, this._createThunderstormParticle)
+    this.addChild(this._thunderstormParticle)
+  }
+
+  /**
+   * Empty draw method for the sky entity.
+   */
+  public draw(): void {}
+}
