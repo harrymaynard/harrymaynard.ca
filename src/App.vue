26ef46a8d114b9c2838cc369d78156b33ac0fd17
Harry Maynard <maynard321@gmail.com>
2025-12-31 08:10:05 -0800

    feat: replace Libertinus Serif with Manrope font and update styles across the application

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,3 @@ import ModalManager from '@/components/ModalManager.vue'
     <ModalManager />
   </div>
 </template>
-
-<style lang="scss">
-@font-face {
-  font-family:"Manrope";
-  src: url("/fonts/Manrope-Regular.woff2") format("woff2");
-}
-
-@font-face {
-  font-family: "Futura BT";
-  src: url("/fonts/futurabt-light-webfont.woff2");
-}
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: Manrope, sans-serif;
-  -webkit-font-smoothing: subpixel-antialiased;
-  -moz-osx-font-smoothing: subpixel-antialiased;
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-}
-body {
-  cursor: url('/images/cursors/cursor-24.png') 3 0, default;
-}
-</style>
